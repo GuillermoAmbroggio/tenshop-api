f316d34af2f55e47a614b25761054a4993d2e006
GuillermoAmbroggio <guillermoambroggio@gmail.com>
2021-04-16 14:33:51 -0300

    Fix dbb

--- src/db.js
+++ src/db.js
@@ -10,6 +10,16 @@ const sequelize = new Sequelize(
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: process.env.DATABASE_DEV
+      ? {}
+      : {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+          },
+        },
   }
 );
 const basename = path.basename(__filename);
