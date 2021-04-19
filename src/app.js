f41ac6dc0746bef3411bd679b7e119a42979ca76
GuillermoAmbroggio <guillermoambroggio@gmail.com>
2021-04-18 22:31:57 -0300

    fix db 5

--- src/app.js
+++ src/app.js
@@ -69,6 +69,7 @@ server.use(
       },
     }),
     secret: "keyboard cat",
+    proxy: true,
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: (30 * 24 * 60 * 60 * 1000) / 3, secure: true }, // 10 days
